755b7325872211523ad4b489bef79727b6aee3c9
Nahtan <nathando13403@gmail.com>
2024-04-21 21:45:38 -0700

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //call controller view method, use view template to generate html response
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        //Get: /HelloWorld/Welcome/
+        //Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["message"] = "hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
